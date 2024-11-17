dd0b801ace1b9d08307c8e5d0a91c6b9017fca11
lfhwnqe <lfhwnqea@gmail.com>
2024-11-17 22:50:25 +0800

    feature: 添加koa

--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
+import { addAliases } from "module-alias";
+addAliases({
+  "@root": __dirname,
+  "@interfaces": `${__dirname}/interface`,
+  "@config": `${__dirname}/config`,
+  "@middlewares": `${__dirname}/middlewares`,
+});
 import Koa from "koa";
+import config from "@config/index";
 const app = new Koa();
+
+const { port, viewDir, memoryFlag, staticDir } = config;
 
 // response
 app.use((ctx) => {
   ctx.body = "Hello Koa";
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log("BFF启动成功");
+});
