03755d22e8649b92999b24d4ea38bdb8048dbad9
lfhwnqe <lfhwnqea@gmail.com>
2024-11-17 22:58:52 +0800

    feature: 引入awalix实现依赖注入

--- app.ts
+++ app.ts
@@ -7,14 +7,21 @@ addAliases({
 });
 import Koa from "koa";
 import config from "@config/index";
+import { createContainer, Lifetime } from 'awilix';
+import { loadControllers, scopePerRequest } from 'awilix-koa';
 const app = new Koa();
 
 const { port, viewDir, memoryFlag, staticDir } = config;
 
-// response
-app.use((ctx) => {
-  ctx.body = "Hello Koa";
+const container = createContainer();
+container.loadModules([`${__dirname}/services/*.ts`], {
+  formatName: 'camelCase',
+  resolverOptions: {
+    lifetime: Lifetime.SCOPED,
+  },
 });
+app.use(scopePerRequest(container));
+app.use(loadControllers(`${__dirname}/routers/*.ts`));
 
 app.listen(port, () => {
   console.log("BFF启动成功");
